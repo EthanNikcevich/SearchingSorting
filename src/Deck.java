a3889b0cb87fdfdf5092ce5eb3aa9b14db8d4a6b
Ethan Nikcevich <ethannikcevich@students.berkeley.net>
2017-11-21 09:59:16 -0800

    Initial commit

---
+++ src/Deck.java
@@ -0,0 +1,188 @@
+import javax.sound.midi.Soundbank;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Collections;
+
+public class Deck {
+    private Random deal;
+    public static ArrayList<Card> allCards = new ArrayList<>();
+    public static ArrayList<Card> hand = new ArrayList<>();
+
+    public Deck(Boolean x) {
+        if (x = true) {
+            for (int k = 1; k < 53; k++) {
+                if (k < 14) {
+                    Card card = new Card(1, k);
+                    allCards.add(card);
+                }
+                else if (k < 27) {
+                    Card card = new Card(2, k - 13);
+                    allCards.add(card);
+                }
+                else if (k < 40) {
+                    Card card = new Card(3, k - 26);
+                    allCards.add(card);
+                } else {
+                    Card card = new Card(4, k - 39);
+                    allCards.add(card);
+                }
+            }
+        } else if (x = false) {
+            for (int k = 1; k < 14; k++) {
+                Card card = new Card(1, k);
+                allCards.add(card);
+            }
+        }
+    }
+    public void swap(int first, int second) {
+        Collections.swap(hand, first, second);
+    }
+
+    public ArrayList<Card> getList() {
+        return hand;
+    }
+
+    public String dealHand(int numCards){
+        for (int k =0; k < numCards; k++) {
+            hand.add(allCards.get(k));
+            System.out.println(allCards.get(k).name);
+        }
+        return "";
+    }
+
+    public String listHand(){
+        for (int k = 0; k < hand.size(); k++) {
+            System.out.println(hand.get(k).name);
+        }
+        return "";
+    }
+
+    public void shuffleDeck(){
+        Collections.shuffle(allCards);
+        System.out.println(allCards);
+    }
+    public void bubbleSortHand(){
+        for(int k=0; k<hand.size(); k++){
+            for (int i = 0; i < hand.size()-1; i++) {
+                if (hand.get(i).value > hand.get(i + 1).value) {
+                    swap(i, i + 1);
+                }
+            }
+        }
+    }
+
+    public void selectionSortHand(){
+        for(int i=0; i<hand.size(); i++){
+            int pos=i;
+            for(int j=i;j<hand.size();j++){
+                if (hand.get(j).value < hand.get(pos).value) {
+                    pos = j;
+                }
+            }
+            Card min=hand.get(pos);
+            hand.set(pos, hand.get(i));
+            hand.set(i, min);
+        }
+    }
+
+
+    public ArrayList<Card> mergeSortHand(ArrayList<Card> whole) {
+        ArrayList<Card> left = new ArrayList<Card>();
+        ArrayList<Card> right = new ArrayList<Card>();
+        int center;
+        if (whole.size() == 1) {
+            return whole;
+        }else{
+            center = whole.size()/2;
+            for (int i=0; i<center; i++) {
+                left.add(whole.get(i));
+            }
+            for (int i=center; i<whole.size(); i++) {
+                right.add(whole.get(i));
+            }
+            left  = mergeSortHand(left);
+            right = mergeSortHand(right);
+            merge(left, right, whole);
+        }
+        return whole;
+    }
+    private void merge(ArrayList<Card> left, ArrayList<Card> right, ArrayList<Card> whole) {
+        int leftIndex = 0;
+        int rightIndex = 0;
+        int wholeIndex = 0;
+        while (leftIndex < left.size() && rightIndex < right.size()) {
+            if ((left.get(leftIndex).value.compareTo(right.get(rightIndex).value)) < 0) {
+                whole.set(wholeIndex, left.get(leftIndex));
+                leftIndex++;
+            }else {
+                whole.set(wholeIndex, right.get(rightIndex));
+                rightIndex++;
+            }
+            wholeIndex++;
+        }
+        ArrayList<Card> rest;
+        int restIndex;
+        if(leftIndex >= left.size()){
+            rest = right;
+            restIndex = rightIndex;
+        }else {
+            rest = left;
+            restIndex = leftIndex;
+        }
+        for (int i=restIndex; i<rest.size(); i++) {
+            whole.set(wholeIndex, rest.get(i));
+            wholeIndex++;
+        }
+    }
+
+
+//    public static ArrayList<Card> leftHalf(ArrayList<Card> array) {
+//        int size = array.size()/2;
+//        ArrayList<Card> left = new ArrayList<>(size);
+//        for (int i = 0; i < size; i++) {
+//            left.get(i) = array.get(i);
+//        }
+//        return left;
+//    }
+//    public static ArrayList<Card> rightHalf(Card[] array) {
+//        int size = array.length / 2;
+//        int sizeB = array.length - size;
+//        Card[] right = new Card[sizeB];
+//        for (int i = 0; i <= sizeB - 1; i++) {
+//            right[i] = array[i - 1 + sizeB];
+//        }
+//        return right;
+//    }
+
+//    public void binarySearchHand(int suit, int face){
+//        binarySearch(hand, suit, face);
+//    }
+//
+    public void binarySearch(int suit, int face){
+        binarySearch2(hand,suit,face);
+    }
+
+    public void binarySearch2(ArrayList<Card> hand, int suit, int face){
+        if(suit==hand.get(hand.size()/2).suit && face==hand.get(hand.size()/2).value){
+            System.out.println("Found: " + hand.get(hand.size()/2).name);
+        }
+        else if(suit<hand.get(hand.size()/2).suit || face<hand.get(hand.size()/2).value){
+            ArrayList<Card> leftHalf = new ArrayList<Card>();
+            System.out.println(hand.size());
+            for(int i=0; i<=hand.size()/2; i++){
+                leftHalf.add(hand.get(i));
+            }
+            binarySearch2(leftHalf,suit,face);
+        }
+        else if(suit>hand.get(hand.size()/2).suit || face>hand.get(hand.size()/2).value){
+            ArrayList<Card> rightHalf = new ArrayList<Card>();
+            for(int i=hand.size()/2; i<=hand.size(); i++){
+                rightHalf.add(hand.get(i));
+            }
+            binarySearch2(rightHalf,suit,face);
+        }
+        else if(hand.size()==1){
+            System.out.println("That card isn't in your hand");
+        }
+    }
+}
---
+++ src/Runner.java
@@ -0,0 +1,13 @@
+public class Runner {
+    public static void main(String[] args){
+        Deck d = new Deck(true);
+        d.shuffleDeck();
+        d.dealHand(7);
+        d.bubbleSortHand();
+        //d.selectionSortHand();
+        //d.mergeSortHand(d.getList());
+        //d.listHand();
+        d.binarySearch(2,3);
+    }
+}
+
